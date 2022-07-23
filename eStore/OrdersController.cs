33d69bb3e86cb5ccaceb6c58cbd4bdc48d6d1843
Trần Tú Anh <anhttse150289@fpt.edu.vn>
2022-07-24 01:22:51 +0700

    orderController

---
+++ eStore/OrdersController.cs
@@ -0,0 +1,119 @@
+﻿using BusinessObject.Models;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eStore
+{
+    public class OrdersController : Controller
+    {
+        IOrderRepository orderRepository = null;
+        public OrdersController() => orderRepository = new OrderRepository();
+        // GET: OrdersController
+        public ActionResult Index()
+        {
+            var orderList = orderRepository.GetAllOrder();
+            return View(orderList);
+        }
+
+        // GET: OrdersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrdersController/Create
+        public ActionResult Create() => View();
+
+        // POST: OrdersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Order order)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    orderRepository.InsertOrder(order);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(order);
+            }
+        }
+
+        // GET: OrdersController/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id != null)
+            {
+                return NotFound();
+            }
+            var order = orderRepository.GetOrderByID(id.Value);
+            if(order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: OrdersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Order order)
+        {
+            try
+            {
+                if(id != order.Id)
+                {
+                    return NotFound();
+                }
+                if (ModelState.IsValid)
+                {
+                    orderRepository.UpdateOrder(order);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: OrdersController/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var order = orderRepository.GetOrderByID(id.Value);
+            if(order == null)
+            {
+                return NotFound();
+            }
+           return View(order);
+        }
+
+        // POST: OrdersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Order order)
+        {
+            try
+            {
+                orderRepository.DeleteOrder(order);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+    }
+}
