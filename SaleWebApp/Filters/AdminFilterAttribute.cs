a702a094fce8dbc0b7618eebecfcea8c864209b3
Hungpd170501 <hungpd170501@gmail.com>
2022-07-24 09:16:34 +0700

    add filter

---
+++ SaleWebApp/Filters/AdminFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SaleWebApp.Filters
+{
+    public class AdminFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string jsonUser = context.HttpContext.Session.GetString("User");
+            string role = context.HttpContext.Session.GetString("Role");
+            if (jsonUser == null || role == null || role != "ADMIN")
+            {
+                context.Result = new NotFoundResult();
+            }
+        }
+    }
+}
